dc89ff47783bc4ae9cb1be49161a24023a272d51
Nitish280 <nsrccs@gmail.com>
2017-06-08 14:35:20 +0530

    Create Student.java

--- src/Student.java
+++ src/Student.java
@@ -1,12 +1,12 @@
 
-public class Student { 
+public class Student { //creating the student class here
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Student s1=new Student();
-		Student s2=new Student();
-		  System.out.println(s1.hashCode());  
-		  System.out.println(s2.hashCode());  
+		Student s1=new Student();//creating an object of Student 
+		Student s2=new Student();//creating an object of Student 
+		  System.out.println(s1.hashCode()); //accessing 1st object member through hashcode reference variable   
+		  System.out.println(s2.hashCode());  //accessing 2nd object member through hashcode reference variable  
 
 	}
 
