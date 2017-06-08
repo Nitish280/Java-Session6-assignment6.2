6d85f22845ac4ab1975420f68ee14d955cd6eaf5
Nitish280 <nsrccs@gmail.com>
2017-06-08 14:30:29 +0530

    Hash

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student { 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s1=new Student();
+		Student s2=new Student();
+		  System.out.println(s1.hashCode());  
+		  System.out.println(s2.hashCode());  
+
+	}
+
+}
